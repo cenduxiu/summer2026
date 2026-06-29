3d39348b5edb2e6a77f0ce9196e3036ee26b3bb2
cenduxiu <332742841@qq.com>
2026-06-29 17:12:00 +0800

    day8:C language basics - variables,printf,scanf,temperature converter

---
+++ day8_c_basics/format_demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int age = 18;
+	float height = 175.5;
+	char grade = 'A';
+
+	printf("wodenianlingshi%d18\n",age);
+	printf("wodeshengaoshi%.1f170\n",height);
+	printf("wodechengjishi%d\n",grade);
+	int input_num;
+	printf("qingshuruyigezhengshu: ");
+	scanf("%d",&input_num);
+	printf("nishurudeshushi: %d\n",input_num);
+
+	return 0;
+}
