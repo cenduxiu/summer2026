7b3ac12e531c844d1d2902404eaefe3ef011ebe6
cenduxiu <332742841@qq.com>
2026-06-30 17:42:29 +0800

    day9: for and while loops - multiplication table

---
+++ day9_loops/love.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int fahrenheit;
+printf("请说出你觉得能够让我们永远在一起的幸运数字: ");
+scanf("%d", &fahrenheit);
+if (fahrenheit = 520) {
+printf("我们注定永远在一起%d",fahrenheit);
+}
+return 0;
+}
